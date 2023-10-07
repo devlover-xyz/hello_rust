8116c37593110d656b76b109ec9a4de5d8f77b0c
aviq baihaqy <aviq.baihaqy@gmail.com>
2023-10-07 16:24:51 +0700

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,26 @@
-// #[derive(Debug)]
-// struct Person<'a> {
-//     name: &'a str,
-//     age: u8,
-// }
-// mod foo;
-
 fn main() {
-    let mut message_number: i8; // address A1
+    let numerik1 = 24;
+    let numerik2: i8 = 20;
+    let numerik3: i16 = 32767;
 
-    message_number = 1;
-    let message1 = "hello";
-    println!("message number {}: {}", message_number, message1);
+    println!("{} | {} | {}", numerik1, numerik2, numerik3);
 
-    message_number = 2;
-    let message2 = "world";
-    println!("message number {}: {}", message_number, message2);
+    let fp1: f32 = 3.14;
+    let fp2: f32 = 3.1415926535;
 
-    let (var1, var2, mut var3): (i8, &str, char) = (24, "hello", 'A');
+    println!("{} | {:.4}", fp1, fp2);
+    // output ==> 3.14 | 3.14159
 
-    println!("var1: {0}", var1); // hasilnya => var1: 24
-    println!("var2: {0}", var2); // hasilnya => var2: hello
-    println!("var3: {0}", var3); // hasilnya => var3: A
+    let var5 = r#"
+        {
+            "name": "tim drake",
+            "gender": "male"
+        }
+    "#;
+    println!("{}", var5);
 
-    var3 = 'C';
-    println!("var3: {0}", var3); // hasilnya => var3: A
+    const LABEL: &str = "nilai pi adalah:";
+    const PI: f32 = 22.0 / 7.0;
 
-    let data1 = 24_i8;
-    println!("data1: {0}", data1); // hasilnya => data1: 24
-
-    let x = 5; // address C1
-    println!("x: {}", x); // hasilnya => x: 5
-
-    let x = x + 1; // address C2
-    println!("x: {}", x); // hasilnya => x: 6
+    println!("{} {}", LABEL, PI);
 }
