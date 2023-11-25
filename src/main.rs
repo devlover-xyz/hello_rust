42d4e4eb202b887a7bb903dd73f1dd8aba400495
aviq baihaqy <aviq.baihaqy@gmail.com>
2023-11-25 10:37:55 +0700

    update data

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,21 @@
+struct User {
+    name: String,
+    sign_in_count: u64,
+    affliation: Vec<String>,
+    active: bool,
+}
+
 fn main() {
-    // let mut counter = 0; // inisialisasi nilai counternya menjadi 0
-    // loop {
-    //     counter +=1; // tambahakan value counter dengan 1
-    //     println!("The counter is now: {}", counter);
-    //     if counter == 100 { // hentikan perulangan saat counter == 5
-    //         break;
-    //     }
-    // }
-
-    // let mut counter = 0;
-    // let mut counter2 = 0;
-    // println!("Now entering the first loop.");
-
-    // 'first_loop: loop {
-    //     // Berikan nama kepada loop yang pertama
-    //     counter += 1;
-    //     println!("The counter is now: {}", counter);
-
-    //     if counter > 9 {
-    //         // Buat loop kedua di dalam loop ini
-    //         println!("Now entering the second loop.");
-
-    //         'second_loop: loop {
-    //             // sekarang kita berada pada 'second_loop
-    //             println!("The second counter is now: {}", counter2);
-    //             counter2 += 1;
-    //             if counter2 == 3 {
-    //                 break 'first_loop; // Hentikan 'first_loop sehingga kita bisa keluar dari program
-    //             }
-    //         }
-    //     }
-    // }
-
-    // warning number karena tidak di gunakan
-    // for _ in 0..3 {
-    //     println!("Printing the same thing three times");
-    // }
-
-    let mut counter = 5;
-    let mut modd;
-
-    let my_number = loop {
-        counter += 1;
-
-        modd = counter % 53;
-        println!("{} => {}", counter, modd);
-
-        if counter % 53 == 3 {
-            break counter;
-        }
+    let user_one = User {
+        name: String::from("Ajmad"),
+        sign_in_count: 12,
+        affliation: vec![
+            String::from("Warchief of the Horde"),
+            String::from("Blackrock Chieftain"),
+            String::from("The Doomhammer"),
+        ],
+        active: false,
     };
-    println!("MY NUMBER : {}", my_number);
+    println!("name: {}", user_one.name);
+    println!("sign-in count: {}", user_one.sign_in_count);
 }
