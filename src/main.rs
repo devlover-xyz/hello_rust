e4d3ce328bdfbae3ef2db099a616117938d35fdb
aviq baihaqy <aviq.baihaqy@gmail.com>
2023-11-25 10:38:17 +0700

    add pointer

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,7 @@
-struct User {
-    name: String,
-    sign_in_count: u64,
-    affliation: Vec<String>,
-    active: bool,
-}
-
 fn main() {
-    let user_one = User {
-        name: String::from("Ajmad"),
-        sign_in_count: 12,
-        affliation: vec![
-            String::from("Warchief of the Horde"),
-            String::from("Blackrock Chieftain"),
-            String::from("The Doomhammer"),
-        ],
-        active: false,
-    };
-    println!("name: {}", user_one.name);
-    println!("sign-in count: {}", user_one.sign_in_count);
+    let number: i32 = 24;
+    println!("value: {:?}", number);
+
+    let pointer_number: &i32 = &number;
+    println!("pointer: {:p}", pointer_number);
 }
