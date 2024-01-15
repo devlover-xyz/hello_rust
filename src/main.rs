f021fb873ad37d022929d6aad8ca1eb663965c9e
aviq baihaqy <aviq.baihaqy@gmail.com>
2024-01-15 10:20:13 +0700

    update pointer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 fn main() {
-    let number: i32 = 24;
-    println!("value: {:?}", number);
-
-    let pointer_number: &i32 = &number;
-    println!("pointer: {:p}", pointer_number);
+    let a = String::from("hello rust");
+    let b = a;
+    println!("a: {:?}, b: {:?}", a, b);
 }
